85babe80da8923a6159dc466ef95d2395396cd31
Simon Strassl <stuff@sigmoid.at>
2016-10-06 21:38:04 +0200

    Support TISS login page

--- tiss-autologin.js
+++ tiss-autologin.js
@@ -27,7 +27,7 @@ var PASSWORD = null;
 })();
 
 function attemptTuwelLogin() {
-    var form = document.querySelector("form#zid_login");
+    var form = document.querySelector('form#zid_login');
 
     if (form !== null) {
         console.log('Found login button, logging in');
@@ -38,7 +38,20 @@ function attemptTuwelLogin() {
 }
 
 function attemptTISSLogin() {
-    var loginButton = document.querySelector(".toolLogin");
+    // First check if we are at the login page
+    if (location.pathname == '/admin/authentifizierung') {
+        console.log('At TISS authentication page - checking for login link');
+        var loginLink = document.querySelector('a.anmelden');
+        if (loginLink !== null) {
+            console.log('Login link found, we are done here');
+            loginLink.click();
+            return;
+        } else {
+            console.log('No login link found');
+        }
+    }
+    // Only if it's not: Check if the button is present
+    var loginButton = document.querySelector('.toolLogin');
     if (loginButton !== null) {
         console.log('Found login button, logging in');
         loginButton.click();
@@ -48,17 +61,17 @@ function attemptTISSLogin() {
 }
 
 function attemptZidLogin() {
-    var form = document.querySelector("form");
+    var form = document.querySelector('form');
 
     if (form !== null) {
-        console.log("Filling form");
-        var name = document.querySelector("input[name='name']");
-        var password = document.querySelector("input[type=password]");
+        console.log('Filling form');
+        var name = document.querySelector('input[name="name"]');
+        var password = document.querySelector('input[type=password]');
         name.value = NAME;
         password.value = PASSWORD;
-        console.log("Submitting form");
+        console.log('Submitting form');
         form.submit();
     } else {
-        console.log("Couldn't find form to submit");
+        console.log('Couldn\'t find form to submit');
     }
 }
